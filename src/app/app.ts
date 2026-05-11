60156ca96e5a853781c9a862f28c020283b4868b
Sergiy Belinskiy <privat.sergiyko2@gmail.com>
2026-05-11 21:05:44 +0300

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {ProfileCard} from './common-ui/profile-card/profile-card';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ProfileCard],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('Angular-app');
+}
