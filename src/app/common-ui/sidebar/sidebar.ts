11f5add3e19dacd239f36704093a6f56f7eacf1c
Sergiy Belinskiy <privat.sergiyko2@gmail.com>
2026-05-19 20:02:19 +0300

    img-1

--- src/app/common-ui/sidebar/sidebar.ts
+++ src/app/common-ui/sidebar/sidebar.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {SvgIcon} from '../svg-icon/svg-icon';
+import {NgForOf} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   imports: [
-    SvgIcon
+    SvgIcon,
+    NgForOf,
+    RouterLink
   ],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
-
+  menuItems = [
+    {
+      label: "Моя страница",
+      icon: "home",
+      link: ""
+    },
+    {
+      label: "Чати",
+      icon: "chats",
+      link: "chats"
+    },
+    {
+      label: "Поиск",
+      icon: "search",
+      link: "search"
+    }
+  ]
 }
